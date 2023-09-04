d993e7807d74b175deb1b34b478a1fc33c15b263
Your Git Username <avinashk.ug20.ece@nitp.ac.in>
2023-09-05 00:46:34 +0530

    nayka update

--- src/Components/AppFooter/index.js
+++ src/Components/AppFooter/index.js
@@ -3,7 +3,7 @@ import { Typography } from "antd";
 function AppFooter() {
   return (
     <div className="AppFooter">
-      <Typography.Link href="tel:+123456789">+123456789</Typography.Link>
+      <Typography.Link href="tel:+123456789">+91-6202057571</Typography.Link>
       <Typography.Link href="https://www.google.com" target={"_blank"}>
         Privacy Policy
       </Typography.Link>
